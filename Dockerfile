af8694b8188b7f17a75563fac3ce8c65c369b88e
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-09 12:40:36 +0100

    Add dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM pudo/scraper-base
+MAINTAINER Friedrich Lindenberg <friedrich@pudo.org>
+
+RUN apt-get install -y gdal-bin python-gdal
+
+COPY . /scraper
+WORKDIR /scraper
+RUN pip install -r requirements.txt
+RUN pip install -e
+CMD sh run.sh
--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ fi
 
 URL=ftp://guest:guest@ted.europa.eu/daily-packages/
 
+DATA_PATH=$DATA_PATH/ted
 SOURCE_PATH=$DATA_PATH/sources
 mkdir -p $SOURCE_PATH
 wget -c -t 5 -nc -nH -P $SOURCE_PATH -r -l 5 --cut-dirs 1 --accept .tar.gz $URL
+
+python ted/parse.py
