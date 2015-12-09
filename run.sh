146856747bd7c8806a77619ee373e715bb075ccd
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-09 12:14:50 +0100

    Download TED files from FTP.

---
+++ run.sh
@@ -0,0 +1,10 @@
+if [ -z "$DATA_PATH" ]; then
+    echo "Need to set DATA_PATH"
+    exit 1
+fi
+
+URL=ftp://guest:guest@ted.europa.eu/daily-packages/
+
+SOURCE_PATH=$DATA_PATH/sources
+mkdir -p $SOURCE_PATH
+wget -c -t 5 -nc -nH -P $SOURCE_PATH -r -l 5 --cut-dirs 1 --accept .tar.gz $URL
